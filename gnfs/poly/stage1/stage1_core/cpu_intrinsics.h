c9534bfd4fcd2ae3852edbdf43e259786b9df049
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2010-02-21 22:09:09 +0000

    add more functions, stub out the rest until later
    git-svn-id: https://msieve.svn.sourceforge.net/svnroot/msieve/trunk@223 8c02911e-0617-4013-80d0-c3bcbcae04d5

--- gnfs/poly/stage1/stage1_core/cpu_intrinsics.h
+++ gnfs/poly/stage1/stage1_core/cpu_intrinsics.h
@@ -22,6 +22,33 @@ extern "C"
 {
 #endif
 
+#ifdef _MSC_VER
+
+#include <intrin.h>
+#pragma intrinsic(__emulu)
+
+#define PROD32(hi, lo, a, b)		\
+	{	uint64 __t = __emulu(a,b);	\
+		hi = (uint32)(__t >> 32);	\
+		lo = (uint32)(__t); }
+
+#elif defined(GCC_ASM32X)
+
+#define PROD32(hi, lo, a, b) \
+	asm("mull %2  \n\t"      \
+	    :"=d"(hi), "=a"(lo)  \
+	    :"%rm"(a), "1"(b)    \
+	    :"cc")
+
+#else
+
+#define PROD32(hi, lo, a, b) \
+	{ uint64 t = (uint64)(a) * (b); \
+	  hi = (uint32)(t >> 32);	\
+	  lo = (uint32)(t); }
+
+#endif
+
 /*------------------------ Comparison ---------------------------------*/
 
 static INLINE int32
@@ -69,39 +96,6 @@ cmp128(uint128 a, uint128 b)
 	return 0;
 }
 
-/*-------------------- Addition/Subtraction --------------------------*/
-static INLINE uint96
-add96(uint96 a, uint96 b)
-{
-	uint96 res;
-
-	return res;
-}
-
-static INLINE uint96
-sub96(uint96 a, uint96 b)
-{
-	uint96 res;
-
-	return res;
-}
-
-static INLINE uint128
-add128(uint128 a, uint128 b)
-{
-	uint128 res;
-
-	return res;
-}
-
-static INLINE uint128
-sub128(uint128 a, uint128 b)
-{
-	uint128 res;
-
-	return res;
-}
-
 /*----------------- Squaring ----------------------------------------*/
 
 static INLINE uint64 
@@ -115,15 +109,47 @@ wide_sqr48(uint64 a)
 {
 	/* a < 2^48 */
 
+	uint32 a0 = (uint32)a;
+	uint32 a1 = (uint32)(a >> 32);
+	uint64 acc;
+	uint32 prod_lo, prod_hi;
 	uint96 res;
+
+	PROD32(prod_hi, prod_lo, a0, a0);
+	res.w[0] = prod_lo;
+	acc = (uint64)prod_hi;
+
+	PROD32(prod_hi, prod_lo, a0, a1);
+	acc += 2 * ((uint64)prod_hi << 32 | prod_lo);
+	res.w[1] = (uint32)acc;
+	res.w[2] = (uint32)(acc >> 32) + a1 * a1;
+
 	return res;
 }
 
 static INLINE uint128
 wide_sqr64(uint64 a)
 {
-
+	uint32 a0 = (uint32)a;
+	uint32 a1 = (uint32)(a >> 32);
+	uint64 acc;
+	uint32 prod_lo, prod_hi;
 	uint128 res;
+
+	PROD32(prod_hi, prod_lo, a0, a0);
+	res.w[0] = prod_lo;
+	acc = (uint64)prod_hi;
+
+	PROD32(prod_hi, prod_lo, a0, a1);
+	acc = acc + prod_lo + prod_lo;
+	res.w[1] = (uint32)acc;
+	acc = (acc >> 32) + prod_hi + prod_hi;
+
+	PROD32(prod_hi, prod_lo, a1, a1);
+	acc += (uint64)prod_hi << 32 | prod_lo;
+	res.w[2] = (uint32)acc;
+	res.w[3] = (uint32)(acc >> 32);
+
 	return res;
 }
 
@@ -135,6 +161,7 @@ modsub64(uint64 a, uint64 b, uint64 p)
 	return mp_modsub_2(a, b, p);
 }
 
+#if 0
 static INLINE uint96
 modsub96(uint96 a, uint96 b, uint96 p) 
 {
@@ -148,39 +175,13 @@ modsub128(uint128 a, uint128 b, uint128 p)
 	uint128 res;
 	return res;
 }
-
-/*------------------- Montgomery arithmetic --------------------------*/
-#ifdef _MSC_VER
-
-#include <intrin.h>
-#pragma intrinsic(__emulu)
-
-#define PROD32(hi, lo, a, b)		\
-	{	uint64 __t = __emulu(a,b);	\
-		hi = (uint32)(__t >> 32);	\
-		lo = (uint32)(__t); }
-
-#elif defined(GCC_ASM32X)
-
-#define PROD32(hi, lo, a, b) \
-	asm("mull %2  \n\t"      \
-	    :"=d"(hi), "=a"(lo)  \
-	    :"%rm"(a), "1"(b)    \
-	    :"cc")
-
-#else
-
-#define PROD32(hi, lo, a, b) \
-	{ uint64 t = (uint64)(a) * (b); \
-	  hi = (uint32)(t >> 32);	\
-	  lo = (uint32)(t); }
-
 #endif
 
+/*------------------- Montgomery arithmetic --------------------------*/
 static INLINE uint64 
 montmul64(uint64 a, uint64 b,
-		uint64 n, uint32 w) {
-
+		uint64 n, uint32 w) 
+{
 	uint32 a0 = (uint32)a;
 	uint32 a1 = (uint32)(a >> 32);
 	uint32 b0 = (uint32)b;
@@ -243,21 +244,25 @@ montmul64(uint64 a, uint64 b,
 		return prod;
 }
 
+#if 0
 static INLINE uint96 
 montmul96(uint96 a, uint96 b,
-		uint96 n, uint32 w) {
-
+		uint96 n, uint32 w) 
+{
 	uint96 res;
 	return res;
+
 }
 
 static INLINE uint128
 montmul128(uint128 a, uint128 b,
-		uint128 n, uint32 w) {
+		uint128 n, uint32 w) 
+{
 
 	uint128 res;
 	return res;
 }
+#endif
 
 /*------------------ Initializing Montgomery arithmetic -----------------*/
 static INLINE uint32 
@@ -271,8 +276,8 @@ montmul32_w(uint32 n) {
 }
 
 static INLINE uint64 
-montmul64_r(uint64 n) {
-
+montmul64_r(uint64 n) 
+{
 	mp_t num, den, rem;
 
 	num.val[0] = 0;
@@ -297,7 +302,29 @@ montmul96_r(uint96 n) {
 
 	/* 2^32 <= n < 2^96 */
 
+	mp_t num, den, rem;
 	uint96 res;
+
+	num.val[0] = 0;
+	num.val[1] = 0;
+	num.val[2] = 0;
+	num.val[3] = 0;
+	num.val[4] = 0;
+	num.val[5] = 0;
+	num.val[6] = 1;
+	num.nwords = 7;
+
+	den.val[0] = n.w[0];
+	den.val[1] = n.w[1];
+	den.val[2] = n.w[2];
+	den.nwords = 3;
+	if (den.val[2] == 0)
+		den.nwords = 2;
+
+	mp_mod(&num, &den, &rem);
+	res.w[0] = rem.val[0];
+	res.w[1] = rem.val[1];
+	res.w[2] = rem.val[2];
 	return res;
 }
 
@@ -306,7 +333,33 @@ montmul128_r(uint128 n) {
 
 	/* 2^64 <= n < 2^128 */
 
+	mp_t num, den, rem;
 	uint128 res;
+
+	num.val[0] = 0;
+	num.val[1] = 0;
+	num.val[2] = 0;
+	num.val[3] = 0;
+	num.val[4] = 0;
+	num.val[5] = 0;
+	num.val[6] = 0;
+	num.val[7] = 0;
+	num.val[8] = 1;
+	num.nwords = 9;
+
+	den.val[0] = n.w[0];
+	den.val[1] = n.w[1];
+	den.val[2] = n.w[2];
+	den.val[3] = n.w[3];
+	den.nwords = 4;
+	if (den.val[3] == 0)
+		den.nwords = 3;
+
+	mp_mod(&num, &den, &rem);
+	res.w[0] = rem.val[0];
+	res.w[1] = rem.val[1];
+	res.w[2] = rem.val[2];
+	res.w[3] = rem.val[3];
 	return res;
 }
 
